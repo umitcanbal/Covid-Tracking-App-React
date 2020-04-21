b4b83f21d45fee678e833af0e4055844b6b47374
umitcanbal <umitcanbal@gmail.com>
2020-04-21 22:41:04 +0200

    Simple Card component is displayed by using Material UI library. NOTE: webpack wasnt bundling the current code, I solved it by writing 'npm install @material-ui/core@next' in command line

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,27 @@
 import React from "react"
 
+import { Card, CardContent, Typography, Grid } from "@material-ui/core"
 
-const Cards = () => {
+import styles from "./Cards.module.css"
+
+const Cards = (props) => {
+
+  console.log(props)
 
   return (
-    <h1>Cardstan yazıyorum!</h1>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Infected</Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="body2">Number of active cases of COVID-19</Typography>
+          </CardContent>
+        </Grid>
+
+      </Grid>
+    </div>
   )
 }
 
